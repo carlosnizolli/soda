5f8ac2c28f84f8793913f74f57b8c057eb6de6fd
carlosnizolli <carlos.barboza@logcomex.com>
2022-11-04 13:41:59 -0300

    teste variable

--- scan.sh
+++ scan.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 export dataset=$1
-export POSTGRES_USERNAME=env_var(POSTGRES_USERNAME)
-export POSTGRES_PASSWORD=env_var(POSTGRES_PASSWORD)
-export SODA_API_PUBLIC=env_var(SODA_API_PUBLIC)
-export SODA_API_PRIVATE=env_var(SODA_API_PRIVATE)
 soda scan -d $1 -c $2 -v DAYS=$3 $4
