a3b76324f9e013cb8c455b1cc2d39d19e225aa36
carlosnizolli <carlos.barboza@logcomex.com>
2023-03-15 17:07:29 -0300

    month

--- scan.sh
+++ scan.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-soda scan -v data_source=$1 -d $1 -c $2 $3
+PREV_2_MONTH=`date --date='-2 month' +'%B'`
+PREV_3_MONTH=`date --date='-3 month' +'%B'`
+PREV_4_MONTH=`date --date='-4 month' +'%B'`
+
+soda scan -v data_source=$1 -d $1 -c $2 -v PREV_2_MONTH=$PREV_2_MONTH -v PREV_3_MONTH=$PREV_3_MONTH -v PREV_4_MONTH=$PREV_4_MONTH $3
 
